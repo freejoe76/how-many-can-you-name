e864f0bc4378ac468f4d06e428f1b5cbafb53a8c
freejoe76 <jmurphy@denverpost.com>
2015-10-06 09:52:13 -0600

    More object migration

--- quiz.js
+++ quiz.js
@@ -22,106 +22,99 @@ var quizzer = {
         this.secs = 0 + secCount(":01");
         this.counter();
     },
-function minCount(input) 
-{
-    // Pull out the minutes part of a time string, as in the "1" part of, say, "1:30"
-    var len = input.length;
-
-    for( var i = 0; i < len; i++ ) if( input.substring(i, i + 1) == ":" ) break;
-
-    return( input.substring(0, i) );
-},
-
-function secCount(input) 
-{
-    // Pull out the seconds part of a time string, as in the "30" part of, say, "1:30"
-    var len = input.length;
-
-    for( var i = 0; i < len; i++ ) if( input.substring(i, i + 1) == ":" ) break;
-
-    return( input.substring(i + 1, input.length) );
-},
-
-function displayTime(mins,secs) 
-{
-    var display;
-
-    if( mins <= 9 ) display = " 0";
-    else display = " ";
-
-    display += mins + ":";
-
-    if( secs <= 9 ) display += "0" + secs;
-    else display += secs;
-
-    return( display );
-},
-
-function counter() 
-{
-    if( correctcount == answerkey.length ) return;
-    secs--;
-    if( secs == -1 ) 
+    minCount: function(input) 
     {
-        secs = 59;
-        mins--;
-    }
-    document.timecount.timer.value = displayTime(mins,secs);
+        // Pull out the minutes part of a time string, as in the "1" part of, say, "1:30"
+        var len = input.length;
 
-    if( ( mins == 0 ) && ( secs == 0 ) ) 
+        for( var i = 0; i < len; i++ ) if( input.substring(i, i + 1) == ":" ) break;
+
+        return( input.substring(0, i) );
+    },
+    secCount: function(input) 
     {
-        window.alert("Time up."); 
-        showAnswers(); 
-    } 
-    else 
+        // Pull out the seconds part of a time string, as in the "30" part of, say, "1:30"
+        var len = input.length;
+
+        for( var i = 0; i < len; i++ ) if( input.substring(i, i + 1) == ":" ) break;
+
+        return( input.substring(i + 1, input.length) );
+    },
+    displayTime: function(mins,secs) 
     {
-        cd = setTimeout("counter()",1000);
-    }
-},
+        var display;
 
+        if( mins <= 9 ) display = " 0";
+        else display = " ";
 
+        display += mins + ":";
 
-function checkAnswer(input)
-{
-    if( input.value.length > 0 )
+        if( secs <= 9 ) display += "0" + secs;
+        else display += secs;
+
+        return( display );
+    },
+    counter: function() 
     {
-        for( var i = 0; i < answerkey.length; i++ )
+        if( correctcount == answerkey.length ) return;
+        secs--;
+        if( secs == -1 ) 
         {
-            if( input.value.toLowerCase() == answerkey[i].toLowerCase() )
+            secs = 59;
+            mins--;
+        }
+        document.timecount.timer.value = displayTime(mins,secs);
+
+        if( ( mins == 0 ) && ( secs == 0 ) ) 
+        {
+            window.alert("Time up."); 
+            showAnswers(); 
+        } 
+        else 
+        {
+            cd = setTimeout("counter()", 1000);
+        }
+    },
+    checkAnswer: function(input)
+    {
+        if( input.value.length > 0 )
+        {
+            for( var i = 0; i < answerkey.length; i++ )
             {
-                correct[correct.length] = answerkey[i];
-                correct.sort();
-                answerkey.splice(i,1);
-                input.value = "";
-                correctcount++;
-                var msg = "";
-                var len_correct = correct.length;
-                answertimes[len_correct] = (mins * 60) + secs;
-                for( var x=0; x < len_correct; x++ ) msg += correct[x]+", ";
-    
-                $("#correct").text(msg);
-                var remainmsg = " remain";
-                
-                $("#remain").text( (answercount - correctcount) + remainmsg );
-                if( correctcount == answercount ) window.alert("You win!"); 
+                if( input.value.toLowerCase() == answerkey[i].toLowerCase() )
+                {
+                    correct[correct.length] = answerkey[i];
+                    correct.sort();
+                    answerkey.splice(i,1);
+                    input.value = "";
+                    correctcount++;
+                    var msg = "";
+                    var len_correct = correct.length;
+                    answertimes[len_correct] = (mins * 60) + secs;
+                    for( var x=0; x < len_correct; x++ ) msg += correct[x]+", ";
+        
+                    $("#correct").text(msg);
+                    var remainmsg = " remain";
+                    
+                    $("#remain").text( (answercount - correctcount) + remainmsg );
+                    if( correctcount == answercount ) window.alert("You win!"); 
+                }
             }
         }
-    }
-    else
+        else
+        {
+            if( input.value == " " ) input.value = "";
+        }
+    },
+    showAnswers: function()
     {
-        if( input.value == " " ) input.value = "";
-    }
-},
- 
- 
-function showAnswers()
-{
-    var len = answerkey.length;
-    var msg = '<h3>Missed:</h3><p>';
-    for( var x=0; x < len; x++ ) msg += answerkey[x]+", ";
-    msg += '</p>';
+        var len = answerkey.length;
+        var msg = '<h3>Missed:</h3><p>';
+        for( var x=0; x < len; x++ ) msg += answerkey[x]+", ";
+        msg += '</p>';
 
-    $("#missed").text(msg);
-    $("#missed").css('display','block');
-    $("#explanation").css('display', 'block');
-},
+        $("#missed").text(msg);
+        $("#missed").css('display','block');
+        $("#explanation").css('display', 'block');
+    }
+}
