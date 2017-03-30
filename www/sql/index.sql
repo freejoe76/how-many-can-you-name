54685f8398de648753897f0b5f9a881de30a0789
freejoe76 <jmurphy@denverpost.com>
2017-03-30 11:49:02 -0400

    Index table adjustments

--- www/sql/index.sql
+++ www/sql/index.sql
@@ -1,8 +1,11 @@
 CREATE TABLE `howmany` (
   `quiz_id` int(10) NOT NULL AUTO_INCREMENT,
+  `slug` VARCHAR(100) NOT NULL,
   `count` int(10) NOT NULL,
   `max` FLOAT NOT NULL,
   `median` FLOAT NOT NULL,
   `mode` FLOAT NOT NULL,
   `mean` FLOAT NOT NULL,
+  `create_date` DATE NOT NULL,
+INDEX `SLUG` (`slug`),
 UNIQUE `INDEX` (`quiz_id`)) ENGINE=InnoDB;
