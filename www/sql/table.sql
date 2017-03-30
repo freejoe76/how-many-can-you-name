741f8b91a5ed45531bce19150bdd060215eb09d0
freejoe76 <jmurphy@denverpost.com>
2017-03-30 15:37:45 -0400

    SQL cleanup

--- www/sql/table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `Interactive`.`howmany_QUIZSLUG` ( `id` INT NOT NULL AUTO_INCREMENT ,
- `ip` VARCHAR(20) NULL ,
- `datetime` TIMESTAMP on update CURRENT_TIMESTAMP() NOT NULL ,
- `correct` INT NULL DEFAULT '0',
- `answer_times` VARCHAR(150) NULL,
- UNIQUE `id` (`id`)) ENGINE = InnoDB;
