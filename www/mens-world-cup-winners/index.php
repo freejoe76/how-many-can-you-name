22213d87970dc89a27a7b5668e6454469e31ee15
ssmirti <ssmirti@nydailynews.com>
2018-07-11 14:38:47 -0400

    updated index.php in marvel movies with avengers and ant man

--- www/mens-world-cup-winners/index.php
+++ www/mens-world-cup-winners/index.php
@@ -38,7 +38,7 @@ if ( $_SERVER['QUERY_STRING'] !== '' ) $append = '?' . substr(htmlspecialchars($
 <!-- EDIT: If you want to give the people more or less than two minutes, edit this. --> 
 <input id="time_limit" type="hidden" value="2" />
 <!-- EDIT: Put the answers here, separated by commas, like this: value="Name1,Name2,Name3,Name4" -->
-<input id="answer_key" type="hidden" value="Uruguay,Italy,Germany,Brazil,England,Argentina,France,Spain" />
+<input id="answer_key" type="hidden" value="Uruguay,Italy,Germany,West Germany,Brazil,England,Argentina,France,Spain" />
 <!-- EDIT: Add the quiz-specific configuration values. -->
 <script>
 var quiz_config = { 
